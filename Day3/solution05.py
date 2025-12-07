96fd2f27e6258e48a68eae774311f5625b43370e
Kronos0815 <nils.schiele@uni-konstanz.de>
2025-12-07 23:21:27 +0100

    Solution 05

--- Day3/solution05.py
+++ Day3/solution05.py
@@ -1,10 +1,29 @@
 import os 
 
-with open ("example.txt", "r") as file:
+with open ("input.txt", "r") as file:
     
     output = 0
     
     for line in file:
-        pass
-        # was zum fick ist n joltage
+
+        line = list(line.strip())
+        line = list(map(int, line))
+        print(line)
+        length = len(line)
+        
+        cominations = []
+        i=0
+        while i < length:
+            j=i+1
+            while j < length:
+                temp = ""
+                a = str(line[i])
+                b = str(line[j])
+                temp = int(temp + a + b)
+                cominations.append(temp)
+                
+                j += 1
+            i += 1
+        output = output + max(cominations)
+                
 print(output)
