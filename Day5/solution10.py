73556474106a69b0fc344c82d51cb7b2c09cc3ed
Kronos0815 <nils.schiele@uni-konstanz.de>
2025-12-17 11:04:19 +0100

    Solution10 Working

--- Day5/solution10.py
+++ Day5/solution10.py
@@ -2,103 +2,128 @@
 def shrink(listToshrink):
     
     input = listToshrink
-
-    freshIDs = []
-    freshIDs.append(input[0])
-
-    for area in input:
-        
-        # neuer Bereich
-        start = int(area[0])
-        end = int(area[1])
+    input.sort()
+    print("sorted",input)
+    while True:
     
-        # checken of neuer bereich sich mit bisherigen bereichen überschneidet:
-        found = False
-        for areaCovered in freshIDs:
+        delete = 0
+        for index in range(0,len(input)):
             
-            oldStart = int(areaCovered[0])
-            oldEnd = int(areaCovered[1])
-            
-            if end >= oldStart and end <= oldEnd:
-                areaCovered[0] = area[0]
-                found = True
-                break
-            elif start <= oldEnd and start >= oldStart:
-                areaCovered[1] = area[1]
-                found = True
-                break
-            elif start <= oldStart and end >= oldEnd:
-                areaCovered[0] = area[0]
-                areaCovered[1] = area[1]
-                found = True
-                break
-            elif start >= oldStart and end <= oldEnd:
-                found = True
+            if index < len(input)-1:
+                currentStart = input[index][0]
+                currentEnd = input[index][1]
+                
+                nextStart = input[index+1][0]
+                nextEnd = input[index+1][1]
+            else: 
                 break
             
-        if not found :
-            freshIDs.append(area)
-    return freshIDs
+            if currentEnd >= nextStart and currentEnd >= nextEnd:
+                input[index+1]=[0,0]
+                delete = delete + 1             
+            elif currentEnd >= nextStart and nextStart >= currentStart:
+                input[index][1] = input[index+1][1]
+                input[index+1]=[0,0]
+                delete = delete + 1
+                # print(delete)
+        
+        print(input)   
+        if delete == 0:
+            break 
+            
+        input.sort()
+        # print(input,delete)
+        input = input[delete:]   
+        # print("cutted",input) 
+        # print("\n")
+        
+        
+    
+    return input
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ## Alte Shrink Funktion 
+    
+    # input = listToshrink
 
+    # freshIDs = []
+    # freshIDs.append(input[0])
 
-inputFromFile = []
+    # for area in input:
+        
+    #     # neuer Bereich
+    #     start = int(area[0])
+    #     end = int(area[1])
+    
+    #     # checken of neuer bereich sich mit bisherigen bereichen überschneidet:
+    #     found = False
+    #     for areaCovered in freshIDs:
+            
+    #         oldStart = int(areaCovered[0])
+    #         oldEnd = int(areaCovered[1])
+            
+    #         if end >= oldStart and end <= oldEnd:
+    #             areaCovered[0] = area[0]
+    #             found = True
+    #             break
+    #         elif start <= oldEnd and start >= oldStart:
+    #             areaCovered[1] = area[1]
+    #             found = True
+    #             break
+    #         elif start <= oldStart and end >= oldEnd:
+    #             areaCovered[0] = area[0]
+    #             areaCovered[1] = area[1]
+    #             found = True
+    #             break
+    #         elif start >= oldStart and end <= oldEnd:
+    #             found = True
+    #             break
+            
+    #     if not found :
+    #         freshIDs.append(area)
+    # return freshIDs
 
 
 print("\n")
 print("----------------------")
 
-with open ("example.txt", "r") as file:
+with open ("input.txt", "r") as file:
+    
+    inputFromFile = []
     
     for line in file:
         
+        temp = []
         if line == "\n":
             break
-        inputFromFile.append(line.strip().split("-"))
-
+        temp.append(line.strip().split("-"))
+        
+        for element in temp:
+            integerElement = []
+            integerElement.append(int(element[0]))
+            integerElement.append(int(element[1]))
+            inputFromFile.append(integerElement)
 
 print(inputFromFile)
 print("----------------------")
 print("\n")
 
-# solange shrinken bis nicht weiter geshrinkt werden kann lul
-x = inputFromFile
-blub = True
+shrinkedIDs = shrink(inputFromFile)
+print(shrinkedIDs)
 
-while blub:
-    y = shrink(x)
+result = 0
+for id in shrinkedIDs:
     
-    if len(x) == len(y):
-        blub = False 
-    else:
-        x = y
-        
-freshIds = x
-print(freshIds)
-print("\n")
+    diff = id[1] - id[0] + 1
+    result+=diff
 
-
-
-# was tu ich hier das ist so schlechter code :(
-amountOfFreshIds = 0    
-
-for uff in freshIds:
-    
-    idsInUff = int(uff[1]) - int(uff[0])+1
-    amountOfFreshIds+=idsInUff
-    
-print(amountOfFreshIds)
-
-
-
-# brute force
-
-""" for area in input:
-    start = int(area[0])
-    end = int(area[1])
-    
-    for i in range(start,end+1):
-        if i not in freshIDs:
-            freshIDs.append(i)
-
-print(freshIDs)
-print(len(freshIDs)) """
+print(result)
